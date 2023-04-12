6ea1c0c18e3ae746f21cf00ce774809edd14b7dc
CallumBoase <callum.boase@gmail.com>
2023-04-12 17:05:53 +1000

    First attempt

--- public/app.js
+++
@@ -1,7 +0,0 @@
-import DataTable from './components/FellowTable.js';
-const {
-  StylesProvider
-} = MaterialUIStyles;
-ReactDOM.render( /*#__PURE__*/React.createElement(StylesProvider, {
-  injectFirst: true
-}, /*#__PURE__*/React.createElement(DataTable, null)), document.getElementById('reactComponent'));
