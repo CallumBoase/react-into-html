2a1964b0be75815765f8d3cb623a927e6378b21c
CallumBoase <callum.boase@gmail.com>
2023-04-13 09:13:30 +1000

    16px

--- components/FellowTable.js
+++ components/FellowTable.js
@@ -4,7 +4,7 @@ import { styled } from '@mui/material/styles';
 import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Skeleton } from '@mui/material';
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
-  padding: '25px !important'
+  padding: '16px !important'
 }));
 
 
