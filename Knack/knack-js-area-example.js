2a2c9ce0536c40200157c084849f9b46c62bd996
CallumBoase <callum.boase@gmail.com>
2023-04-12 18:10:47 +1000

    Replace index.html position

--- Knack/knack-js-area-example.js
+++ Knack/knack-js-area-example.js
@@ -1,23 +1,6 @@
-//External scripts to load into the Knack app
-//Dependencies for React, React-Dom, MUI and knackApiHelper
-const scripts = [
-    { src: 'https://cdn.jsdelivr.net/npm/react/umd/react.production.min.js' },
-    { src: 'https://cdn.jsdelivr.net/npm/react-dom/umd/react-dom.production.min.js' },
-    { src: 'https://cdn.jsdelivr.net/npm/@mui/material@5/umd/material-ui.production.min.js' },
-    { src: 'https://cdn.jsdelivr.net/npm/knack-api-helper@2.1.4/browser.js' },
-];
-
-//Block the app loading while we load the external scripts
-KnackInitAsync = function ($, callback) {
-    // REQUIRED: Explicitly include jQuery
-    window.$ = $;
-
-    loadScripts(scripts, callback, null)
-};
-
 //Assuming that we have a rich text view containing <div id="reactComponent"></div> and it's view_76
 $(document).on('knack-view-render.view_76', function(event, view, record){
-    loadScripts([{ src: 'https://extraordinary-khapse-4c4e7c.netlify.app/app.js', type: 'module'}])
+    loadScripts([{ src: 'https://extraordinary-khapse-4c4e7c.netlify.app/bundle.js'}])
 })
 
 //Helper function to load scripts
