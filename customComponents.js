75a3a46685ef1cde98aabe656c8ac7fdc523fb2f
CallumBoase <callum.boase@gmail.com>
2023-04-13 14:18:25 +1000

    Update theme

--- customComponents.js
+++ customComponents.js
@@ -4,21 +4,8 @@ import ReactDOM from 'react-dom';
 import FellowTable from './components/FellowTable.js';
 import HelloWorld from './components/HelloWorld.js';
 import MultiFileUploader from './components/MultiFileUploader/MultiFileUploader.js';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-
-//Create a custom theme to override MUI defaults
-//We include only what is required to make the MUI styles take precedence over the Knack styles when embedding in a Knack app
-const theme = createTheme({
-  components: {
-    MuiTableCell: {
-      styleOverrides: {
-        root: {
-          padding: '16px !important',
-        },
-      },
-    },
-  },
-});
+import theme from './theme.js';
+import { ThemeProvider } from '@mui/material/styles';
 
 const customComponents = {render: {}}
 
---
+++ theme.js
@@ -0,0 +1,18 @@
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+//Create a custom theme to override MUI defaults
+//We include only what is required to make the MUI styles take precedence over the Knack styles when embedding in a Knack app
+const theme = createTheme({
+    components: {
+        MuiTableCell: {
+            styleOverrides: {
+                root: {
+                    padding: '16px !important',
+                    verticalAlign: 'top !important'
+                },
+            },
+        },
+    },
+});
+
+export default theme;
