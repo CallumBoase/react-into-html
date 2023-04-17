2ed6ab236a6fe4ffc23210923fe60b1931c3f661
CallumBoase <callum.boase@gmail.com>
2023-04-17 18:34:02 +1000

    Readme for Multi File Uploader

--- Examples/example-components/MultiFileUploader/globals.js
+++ Examples/example-components/MultiFileUploader/globals.js
@@ -1,45 +1,36 @@
 //Globals for the example component MultiFileUploader
 //You'd need to configure this for your own Knack app to use the component
+//For instructions on how to configure your Knack app see ./Examples/example-components/MultiFileUploader/readme_multiFileUploader.md
 
 const globals = {
     Knack: {
         
-        //Set to a Knack application ID that you own, that you want to upload files to
-        //And where you want to render the multi-file uploader to
-        applicationId: 'XXXXXXXXXXXX',
+        //Set to your Knack application ID
+        applicationId: '642d26891085670027a17157',
         
         objects: {
 
             fileUploads: {
-                //Set to the correct field_ids within a Knack object called "file uploads"
-                //  -File: a file upload field
-                //  -Category: a multi-choice field
-                //  -Description: a text field
-                //  -Member: a relationship field to a table/object called "members"
+                //Set to the field_ids in your fileUploads Knack object (for fields: file, category, description and member)
                 fields: {
                     file: 'field_29',
                     category: 'field_30',
                     description: 'field_31',
                     member: 'field_33'
                 },
-                //Set to a scene and view_key pair containing an "Add file upload record" form
-                //The form should have the above fields in it
-                //The form should be login protected, limited to the user role/s you want to access to the multi file uploader
+                //Set to the scene and view_key pair containing the "Add file upload" form (for API calls to use)
                 addFileUploadForm: {
                     scene: 'scene_55',
                     view: 'view_78'
                 }
             },
-            //Set to the correct field_ids within a Knack object called "members" with these fields:
-            //  -Member Name: a text field
+            //Set to the field_id in your members Knack object (for field: Name)
             members: {
                 fields: {
                     memberName: 'field_32'
                 },
                 gridOfMembers: {
-                    //Set to a scene and view_key pair containing a grid of members to show in the member dropdown of this component
-                    //The grid should have the above field in it
-                    //The grid should be login protected, limited to the user role/s you want to access to the multi file uploader
+                    //Set to the scene and view_key pair containing the "Grid of members" view (for API calls to use)
                     scene: 'scene_55',
                     view: 'view_82'
                 }
